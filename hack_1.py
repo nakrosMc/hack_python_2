9949f77aeaa09073e976241f90bd1bebb78f6816
nakrosMc <nakrosnc@gmail.com>
2024-03-10 12:47:41 -0400

    a

--- hack_1.py
+++ hack_1.py
@@ -6,7 +6,18 @@ text: "qux" output => "qUx"
 text: "eq" output => "eq" 
 """
 
+def fn_hack_1(txt):
+    new_text = ""
+    
+    for i, text in enumerate(txt):
+        if text == "q":
+           new_text += text.lower()
+        elif i == 1 or i == 4:
+           new_text += text.upper()
+        else:
+           new_text += text
+    return new_text
 
-def fn_hack_1(s):
-    result = s
-    return result
+print(fn_hack_1("fooziman"))
+print(fn_hack_1("qux"))
+print(fn_hack_1("eq"))
