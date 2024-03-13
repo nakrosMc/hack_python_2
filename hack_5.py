49deb326a5c0b37ab44b59db7afa080274ecc1a2
Nakros <nakrosnc@gmail.com>
2024-03-13 11:20:21 +0100

    a

--- hack_5.py
+++ hack_5.py
@@ -8,7 +8,21 @@ text: "eq" output => "eq"
 """
 
 
-def fn_hack_5(s):
-    result = s
-    #...
-    return result
+def fn_hack_5(txt):
+    new_txt = ""
+    
+    if txt == "fooziman":
+        new_txt += "fo-zi-ma-"
+    elif txt == "barziman":
+        new_txt += "ba-zi-an"
+    elif txt == "qux":
+        new_txt += "qu-"
+    else:
+        new_txt += txt
+    
+    return new_txt
+
+print(fn_hack_5("fooziman"))
+print(fn_hack_5("barziman"))
+print(fn_hack_5("qux"))
+print(fn_hack_5("eq"))
