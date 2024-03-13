fcff411af512a0db861a4fd1017e1c6acecaafcd
nakrosMc <nakrosnc@gmail.com>
2024-03-13 11:17:24 -0400

    a

--- hack_3.py
+++ hack_3.py
@@ -15,7 +15,36 @@ text: "qux" output => "QvX"
 """
 
 
-def fn_hack_3(s):
-    result = s
-    #...
+def fn_hack_3(txt):
+    result = ""
+    
+    for text in txt:
+        if text == "a":
+            result += "@"
+        elif text == "e":
+            result += "3"
+        elif text == "q":
+            result += "Q"
+        elif text == "x":
+            result += "X"
+        elif text == "f":
+            result += "F"
+        elif text == "b":
+            result += "B"
+        elif text == "n":
+            result += "N"
+        elif text == "i":
+            result += "¡"
+        elif text == "o":
+            result += "0"
+        elif text == "u":
+            result += "v"
+        else:
+            result += text
     return result
+
+print(fn_hack_3("fooziman"))
+print(fn_hack_3("barziman"))
+print(fn_hack_3("3q"))
+print(fn_hack_3("qu"))
+print(fn_hack_3("qux"))
--- hack_5.py
+++ hack_5.py
@@ -7,8 +7,25 @@ text: "qux" output => "qu-"
 text: "eq" output => "eq" 
 """
 
+def modify_text(text):
+    modified_text = ""
+    vowels = "aeiou"
+    previous_was_vowel = False
+    
+    # Iterar sobre el texto
+    for char in text:
+        if char in vowels:
+            if previous_was_vowel:
+                modified_text += "-"
+            modified_text += char
+            previous_was_vowel = True
+        else:
+            modified_text += char
+            previous_was_vowel = False
+    
+    return modified_text
 
-def fn_hack_5(s):
-    result = s
-    #...
-    return result
+# Ejemplos de uso
+texts = ["fooziman", "barziman", "qux", "eq"]
+for text in texts:
+    print(f"text: \"{text}\" output => \"{modify_text(text)}\"")
