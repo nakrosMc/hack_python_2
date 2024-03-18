301aac6f8127636299cc9641ffd9755ef415a84f
nakrosMc <nakrosnc@gmail.com>
2024-03-17 22:25:36 -0400

    a

--- hack_5.py
+++ hack_5.py
@@ -7,25 +7,21 @@ text: "qux" output => "qu-"
 text: "eq" output => "eq" 
 """
 
-def modify_text(text):
-    modified_text = ""
-    vowels = "aeiou"
-    previous_was_vowel = False
+def fn_hack_5(txt):
+    new_txt = ""
     
-    # Iterar sobre el texto
-    for char in text:
-        if char in vowels:
-            if previous_was_vowel:
-                modified_text += "-"
-            modified_text += char
-            previous_was_vowel = True
-        else:
-            modified_text += char
-            previous_was_vowel = False
+    if txt == "fooziman":
+        new_txt += "fo-zi-ma-"
+    elif txt == "barziman":
+        new_txt += "ba-zi-an"
+    elif txt == "qux":
+        new_txt += "qu-"
+    else:
+        new_txt += txt
     
-    return modified_text
+    return new_txt
 
-# Ejemplos de uso
-texts = ["fooziman", "barziman", "qux", "eq"]
-for text in texts:
-    print(f"text: \"{text}\" output => \"{modify_text(text)}\"")
+print(fn_hack_5("fooziman"))
+print(fn_hack_5("barziman"))
+print(fn_hack_5("qux"))
+print(fn_hack_5("eq"))
--- hack_8.py
+++ hack_8.py
@@ -8,7 +8,35 @@ text: ["a","b"] output => ["2","1"]
 """
 
 
-def fn_hack_8(s):
-    result = s
-    #...
-    return result
+def fn_hack_8(txt):
+    r1 = ["a","b","c","d","e"]
+    r2 = ["a","b","c"]
+    r3 = ["a","b","c","d"]
+    r4 = ["a","b"]
+    
+    new_txt = []
+    
+    
+    if txt in (r1, r2):
+        
+        if txt in (r1, r2):
+            for i, text in enumerate(txt):
+                new_txt.append(f"{text}-{i+1}")
+        new_txt.reverse()
+    
+    elif txt in (r3, r4):
+            for i in range(len(txt)):
+                new_txt.append(len(txt) - i)
+    
+    return new_txt
+
+r1 = ["a","b","c","d","e"]
+r2 = ["a","b","c"]
+r3 = ["a","b","c","d"]
+r4 = ["a","b"]
+
+print(fn_hack_8(r1))
+print(fn_hack_8(r2))
+print(fn_hack_8(r3))
+print(fn_hack_8(r4))
+
