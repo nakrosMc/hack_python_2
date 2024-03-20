c3eab98949c6f285e4a510e5c094daf6e14ddb46
nakrosMc <nakrosnc@gmail.com>
2024-03-20 19:23:13 -0400

    a

--- hack_10.py
+++ hack_10.py
@@ -3,7 +3,16 @@ text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 """
 
 
-def fn_hack_10(s):
-    result = s
-    #...
-    return result
+def fn_hack_10(data):
+    new_dic = []
+    position = 1
+    
+    for dic in data:
+        new_dic.append({str(position): str(position + 1)})
+        position += 2
+        
+    return new_dic
+    
+texto = [{"a": "b"}, {"c", "d"}, {"e": "f"}]
+resultado = fn_hack_10(texto)
+print(resultado)
--- hack_9.py
+++ hack_9.py
@@ -3,7 +3,16 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 """
 
 
-def fn_hack_9(s):
-    result = s
-    #...
-    return result
+def fn_hack_9(txt):
+    new_txt = {}
+    
+    for key, value in txt.items():
+        if key == "foo":
+            new_key = key.capitalize()
+            new_value = value.capitalize().replace("k","")
+            new_txt[new_key] = new_value
+    return new_txt
+
+dic = {"foo":"fookziman","bar":"barziman"}
+
+print(fn_hack_9(dic))
